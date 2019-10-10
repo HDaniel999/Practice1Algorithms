9027652aed15b99085f797e1c093cd01cb321e0b
HDaniel999 <hdaniel999@gmail.com>
2019-10-10 13:02:40 -0500

    This is a second commit. Dog class was added and extra message in main Class was added

--- src/com/practica1algoritmos/app/HolaMundo.java
+++ src/com/practica1algoritmos/app/HolaMundo.java
@@ -4,7 +4,8 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola Mundito");
+		System.out.println("Hola Mundo");
+		System.out.println("Esta es otra linea de codigo");
 
 	}
 
